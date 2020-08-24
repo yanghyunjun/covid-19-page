64512fe81b4566579130ccae291a380ec6eb1c7e
akakak231@gmail.com <marine.jun@kakaocorp.com>
2020-08-24 18:25:59 +0900

    [fix] rendering issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { NextPage } from "next";
-import Main from "../components/Main";
 
 const Container = styled.div``;
 
 const index: NextPage = () => {
-  return <Container>{/* <Main /> */}</Container>;
+  return <Container></Container>;
 };
 
 export default index;
